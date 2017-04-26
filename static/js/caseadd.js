58399cb2f70e8098cc545909d7daa2bc4769bbff
codelieche <codelieche@gmail.com>
2017-04-26 21:24:01 +0800

    1、Case add 和edit页面调整； 2、添加了遮罩功能; 3、添加了组件化事务的方式添加case; 4、此次修改主要针对前端.

---
+++ static/js/caseadd.js
@@ -0,0 +1,41 @@
+var fieldAddClick = function(ele){
+    var e = $('.box').first().clone();
+    e.find('.field').val('');
+    $(ele).parent('.box').after(e);
+};
+
+var fieldDelClick = function(ele){
+    var bodydata = $(ele).parent('.box').parent('.body-data');
+    if(bodydata.find('.box').length > 1){
+        $(ele).parent('.box').remove();
+    }else{
+        alert('至少需要保留一个!');
+    }
+};
+
+var showShiwuMask = function(){
+    // 显示遮罩
+    var mask = $('.mask-shiwu');
+    $('.mask-shiwu').css('height', window.outerHeight + 'px');
+    mask.show();
+    window.scrollTo(0, 0);
+}
+
+
+$(function(){
+   // 关闭mask
+    $('#mask-close').on('click', function(){
+        $('.mask').hide();
+    });
+    $('.code-way').eq(0).show();
+
+    $('#id_way').on('change',function(){
+        if($('#id_way').val() === 'shiwu'){
+            $('.code-way').eq(0).show();
+            $('.code-way').eq(1).hide();
+        }else{
+            $('.code-way').eq(1).show();
+            $('.code-way').eq(0).hide();
+        }
+    });
+});
