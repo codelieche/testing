07bb04f00136188ef55eeb381d2e9c16d1ad62ca
codelieche <codelieche@gmail.com>
2017-05-27 08:48:11 +0800

    add 403 404 405 view function

--- apps/account/views.py
+++ apps/account/views.py
@@ -48,3 +48,14 @@ class LogoutView(View):
         next_url = request.GET.get('next', '/')
         return redirect(next_url)
 
+
+def page_403(request):
+    return render(request, '403.html')
+
+
+def page_404(request):
+    return render(request, '404.html')
+
+
+def page_500(request):
+    return render(request, '500.html')
