6777331a2953754e617c0f2cc2211eea1f2e35ec
codelieche <codelieche@gmail.com>
2017-06-08 15:33:43 +0800

    tproject admin.py register ServerInfo

--- apps/tproject/admin.py
+++ apps/tproject/admin.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 from django.contrib import admin
 
-from .models import Project
+from .models import Project, ServerInfo
 
 # Register your models here.
 
@@ -17,4 +17,15 @@ class ProjectModelAdmin(admin.ModelAdmin):
     list_filter = ('need_test',)
     ordering = ('id',)
 
+
+class ServerModelAdmin(admin.ModelAdmin):
+    """
+    Project Server Info Model Admin
+    """
+    list_display = ('id', 'project', 'test_db', 'product_db', 'test_deployment',
+                    'product_deployment', 'test_server', 'product_server')
+    search_fields = ('test_deployment', 'product_deployment')
+    ordering = ('id',)
+
 admin.site.register(Project, ProjectModelAdmin)
+admin.site.register(ServerInfo, ServerModelAdmin)
